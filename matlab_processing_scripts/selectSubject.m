45ac8c4db57fe1e6d23d7e001b476f3b9c72dd74
Holly Gorin <hg284@shp.rutgers.edu>
2025-05-07 06:35:04 -0400

    Consolidate selectSubject with createSubjectIDMap, update scripts to reflect this, and work on plotting scripts
    - Nested `createSubjectIDMap` inside `selectSubjects` and added `subjectMap` as an output for better efficiency.
    - Updated `trimming.m` and `extractAll.m` to:
        - changed calling selectSubject to include subjectMap as an output so they can use it to map subject IDs back to subject folders
            - for trimming to save trimmed files there
            - for extractall to find .mat files
        - deleted redundant call for createSubjectIDMap
    - Improved efficiency in `extractAll.m` by using `selectTrial` more effectively.
    - Added y-axis unit labels to several plotting scripts for clarity.

--- matlab_processing_scripts/selectSubject.m
+++ matlab_processing_scripts/selectSubject.m
@@ -1,8 +1,9 @@
-%this function allows user to input what subjects to select or, if no
-%subjects are specified, selects them all from createSubjectIDMap function
-
-function selectedSubjects = selectSubject(subjects)
-    %creates selectedSubjects array
+function [selectedSubjects, subjectMap] = selectSubject(subjects)
+    %this function allows user to input what subjects to select (or to run all subjects)
+    %subject IDs are listed for all existing subject folders
+        %nested createSubjectIDMap function pulls all subject folders and assigns simple subject IDs   
+    %creates selectedSubjects array 
+        %(to be looped through in trimming.m and extractall.m)
 
     arguments
         subjects cell = {} %optional- if want to specify specific subjects to process
@@ -26,9 +27,51 @@ end
                 error('1+ subject IDs not found')
             end
     
-            selectedSubjects = subjects;  % if all valid subject IDs
+        % if all selected subject IDs are valid, creates an array of selected subjects
+            selectedSubjects = subjects;  
     end
 end
     
+
+%%NESTED createSubjectIDMap HELPER FUNCTION
+%this function locates all subject folders in the raw data folder and assigns subject IDs to each subject's folder
+function SubjectIDmap = createSubjectIDMap()
+
+    thesisDataAnalysisSettings;  % call script with directories/variables
+
+    rootDir = rawDataFolderDir;
+        %where subject folders are  
     
-  
+    allRawDataFolder = dir(rootDir); 
+        %pulls everything from the raw data folder (folders and files) --> structure array
+    subjectFolders = allRawDataFolder([allRawDataFolder.isdir]); 
+        %isdir gets rid of files and just keeps folders (aka directories)
+    subjectFolders = subjectFolders(~ismember({subjectFolders.name}, {'.', '..'}));
+        %gets rid of 'system entry' folders so just takes subject folders
+    
+    subjectMap = containers.Map();%create empty map
+    for i = 1:length(subjectFolders) %for all subject folders
+        folderName=subjectFolders(i).name;
+            % Extract subject ID from folder name using 'regular expression (aka finds naming pattern):
+        tokens = regexp(folderName, '^(H\d+|S\d+)', 'match');
+            %token = shortcut description (ie: d for digit) 
+            %^ is start of string
+            %(H\d+) = H#(#) and S\d+ = S#(#)
+            %match creates cell array of the matches (ie the subject IDs)
+        if ~isempty(tokens) %ie if finds matches^
+            subjectID = tokens{1};  % Use the match (e.g., 'S1, H11, etc') as the subject ID
+                %{} pulls the actual text content out of the matching cell
+            subjectMap(subjectID) = folderName;
+                % Adds to the map linking subjectID with folderName
+        else
+            error('Subject ID not found in folder name: %s', folderName);
+            % gives an error if a subject's folder doesn't match the pattern
+        end
+    end
+
+    % create the map:
+    SubjectIDmap = subjectMap;
+end
+
+
+
